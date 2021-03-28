df0d4f7e614d14f6f44398230ad905571b71edcd
Iulian <alexnegut@yahoo.com>
2021-03-29 01:41:00 +0300

    putin la offer devices

--- src/components/OfferDevices/OfferDevices.js
+++ src/components/OfferDevices/OfferDevices.js
@@ -3,7 +3,14 @@ import React from 'react';
 const OfferDevices = () => {
   return (
     <div>
-      <h1>Offer devices</h1>
+      <h1 className="offer-title">Telefoane, Tablete, Wearables</h1>
+      <ul className="offer-list">
+        <li className="offer-item"><button> Reluare cu Galaxy Unpacked</button></li>
+        <li className="offer-item"><button> Galaxy S21 | S21+ 5G</button></li>
+        <li className="offer-item"><button> Galaxy A52</button></li>
+        <li className="offer-item"><button> Galaxy A72</button></li>
+        <li className="offer-item"><button> Galaxy Buds Pro</button></li>
+      </ul>
     </div>
   );
 };
